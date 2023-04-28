793417196e5a80e5f8017a35eafff6a5ab91a47c
Ionut Burchi <admin@burconsult.com>
2023-04-28 12:55:26 +0200

    Added check for db exist

--- app/db.py
+++ app/db.py
@@ -1,7 +1,28 @@
+import os
 import sqlite3
 import pandas as pd
 
+def create_database():
+    conn = sqlite3.connect("db/debates.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE debates (
+            debate_id TEXT PRIMARY KEY,
+            topic TEXT,
+            pro_args TEXT,
+            con_args TEXT
+        )
+    """)
+    conn.commit()
+    conn.close()
+
 def save_debate_to_db(debate_id, topic, ip_address, pro_args, con_args):
+        db_path = "db/debates.db"
+    
+        # Check if the database file exists, if not, create the database
+        if not os.path.exists(db_path):
+                create_database()
+
         conn = sqlite3.connect("db/debates.db")
         cursor = conn.cursor()
         cursor.execute('''CREATE TABLE IF NOT EXISTS debates
@@ -14,10 +35,15 @@ def save_debate_to_db(debate_id, topic, ip_address, pro_args, con_args):
         conn.close()
 
 def get_previous_debates():
-    conn = sqlite3.connect("db/debates.db")
-    df = pd.read_sql_query("SELECT * from debates", conn)
-    conn.close()
-    return df
+        db_path = "db/debates.db"
+
+        # Check if the database file exists, if not, create the database
+        if not os.path.exists(db_path):
+                create_database()
+        conn = sqlite3.connect("db/debates.db")
+        df = pd.read_sql_query("SELECT * from debates", conn)
+        conn.close()
+        return df
 
 def delete_debate_from_db(debate_id):
     conn = sqlite3.connect("db/debates.db")
