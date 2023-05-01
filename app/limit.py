40e309e00e54c422f164285f0c5b17f3176e14a6
Ionut John Burchi <admin@burconsult.com>
2023-05-01 15:28:08 +0200

    Fix IP

--- app/limit.py
+++ app/limit.py
@@ -7,7 +7,8 @@ ip_rate_limiters = {}
 def get_client_ip(api_key):
     try:
         response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key={api_key}")
-        ip = str(json.loads(response.text)['ip_address'])
+        result = json.loads(response.content)
+        ip = str(format(result['ip_address']))
         return ip
     except:
         return None
